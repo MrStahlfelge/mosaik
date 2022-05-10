7c56f72ebca3b5e08f270e4b82cea6c630721a69
Benjamin Schulte <bs@golfgl.de>
2022-05-10 18:42:52 +0200

    Desktop demo: live change

--- desktop-demo/src/main/kotlin/org/ergoplatform/mosaik/Main.kt
+++ desktop-demo/src/main/kotlin/org/ergoplatform/mosaik/Main.kt
@@ -1,9 +1,15 @@
 package org.ergoplatform.mosaik
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
@@ -15,27 +21,43 @@ fun main() {
     application {
         val windowState = rememberWindowState()
 
-        val viewTree = ViewTree(
-            UUID.randomUUID().toString()
-        ).apply {
-            setRootView(
-                MosaikSerializer().viewElementFromJson(
-                    this.javaClass.getResource("/default_tree.json")!!.readText()
-                ),
-                0L
-            )
-        }
+        val json = this.javaClass.getResource("/default_tree.json")!!.readText()
 
         Window(
             onCloseRequest = ::exitApplication,
             state = windowState,
             title = "Mosaik Demo"
         ) {
+            val textState = remember { mutableStateOf(TextFieldValue(json)) }
+            val viewTree = try {
+                ViewTree(
+                    UUID.randomUUID().toString()
+                ).apply {
+                    setRootView(
+                        MosaikSerializer().viewElementFromJson(textState.value.text),
+                        0L
+                    )
+                }
+            } catch (t: Throwable) {
+                println(t.message)
+                t.printStackTrace()
+                null
+            }
+
             MaterialTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    renderTreeElement(viewTree.content!!)
+                    Row {
+                        if (viewTree != null) {
+                            renderTreeElement(viewTree.content!!, Modifier.weight(2.0f))
+                        } else {
+                            Box(Modifier.weight(2.0f))
+                        }
+                        TextField(textState.value, onValueChange = {
+                            textState.value = it
+                        }, Modifier.weight(1.0f))
+                    }
                 }
             }
         }
